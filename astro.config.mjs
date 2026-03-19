5b14b59b2da28129e75fa974756e97161df2b41b
David Štrunc <strunc@madfoxdesign.cz>
2026-03-19 16:28:24 +0100

    feat: SEO (structured data, OG, sitemap, robots.txt), Mixpanel, fix logo link

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://blog.ustipcaku.cz',
   output: 'static',
+  integrations: [sitemap()],
 });
